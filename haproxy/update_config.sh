1d52fec0a29e5691f576c118d7ca5252b53c9be0
0xf00f00 <123018047+0xf00f00@users.noreply.github.com>
2023-02-13 23:16:47 +0100

    Allow multiple web domains

--- haproxy/update_config.sh
+++ haproxy/update_config.sh
@@ -2,3 +2,4 @@
 
 cp /root/haproxy/haproxy.cfg /root/haproxy_data/haproxy.cfg
 echo ${SHADOWTLS_DOMAINS} | sed 's/:[0-9]\+//g' | tr ';' '\n' > /root/haproxy_data/shadowtls_domains.lst
+echo ${WEB_DOMAINS} | tr ', ' '\n' > /root/haproxy_data/web_domains.lst
