1266d5229c15d3f438531e527a96e63c64569745
0xf00f00 <123018047+0xf00f00@users.noreply.github.com>
2025-02-05 14:18:01 -0800

    Comment out shadowtls and shadowsocks services in docker-compose and HAProxy configuration for cleanup

--- haproxy/update_config.sh
+++ haproxy/update_config.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cp /root/haproxy/haproxy.cfg /root/haproxy_data/haproxy.cfg
-echo ${SHADOWTLS_DOMAINS} | sed 's/:[0-9]\+//g' | tr ';' '\n' > /root/haproxy_data/shadowtls_domains.lst
+# echo ${SHADOWTLS_DOMAINS} | sed 's/:[0-9]\+//g' | tr ';' '\n' > /root/haproxy_data/shadowtls_domains.lst
 echo ${WEB_DOMAINS} | tr ', ' '\n' > /root/haproxy_data/web_domains.lst
