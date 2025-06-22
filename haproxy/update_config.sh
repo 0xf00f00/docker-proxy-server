dc3ce451243a78ef9c4b890b6ffa63fafb0b2fe6
0xf00f00 <123018047+0xf00f00@users.noreply.github.com>
2025-06-22 14:25:32 -0700

    Add FRP configuration and update HAProxy settings for improved routing

--- haproxy/update_config.sh
+++ haproxy/update_config.sh
@@ -3,3 +3,4 @@
 cp /root/haproxy/haproxy.cfg /root/haproxy_data/haproxy.cfg
 # echo ${SHADOWTLS_DOMAINS} | sed 's/:[0-9]\+//g' | tr ';' '\n' > /root/haproxy_data/shadowtls_domains.lst
 echo ${WEB_DOMAINS} | tr ', ' '\n' > /root/haproxy_data/web_domains.lst
+echo ${FRP_DOMAIN} | tr ', ' '\n' > /root/haproxy_data/frp_domains.lst
